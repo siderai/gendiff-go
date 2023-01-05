4c18aa029d71110cf18690dfd43973e84090e136
Alexander Sidorov <sidai@bk.ru>
2023-01-05 15:59:26 +0300

    feat: Terminal entrypoint

--- gendiff.go
+++ gendiff.go
@@ -1,8 +1,23 @@
 package gendiff
 
-import "fmt"
+import (
+	"os"
+	"path/filepath"
+)
 
-// Hi returns a friendly greeting
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s", name)
+func validate_path(path string) (validated_path string, err error) {
+	return path, nil
+}
+
+func main() {
+	args := os.Args
+	// TODO: Validate paths
+	path_first, err := validate_path(filepath.Base(args[1]))
+	if err != nil {
+		return nil, err
+	}
+	path_second, err := validate_path(filepath.Base(args[2]))
+	if err != nil {
+		return nil, err
+	}
 }
