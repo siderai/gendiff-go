484f9a050047a2f48372d5e9cc038ecff0df958c
Alexander Sidorov <sidai@bk.ru>
2023-01-06 03:17:55 +0300

    feat: Translate file content to map

--- gendiff.go
+++ gendiff.go
@@ -1,8 +1,10 @@
 package gendiff
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"os"
-	"path/filepath"
 )
 
 func validate_path(path string) (validated_path string, err error) {
@@ -10,14 +12,42 @@ func validate_path(path string) (validated_path string, err error) {
 }
 
 func main() {
-	args := os.Args
+	firstFilePath := os.Args[1]
+	secondFilePath := os.Args[2]
+
 	// TODO: Validate paths
-	path_first, err := validate_path(filepath.Base(args[1]))
+
+	// pathFirst, err := validate_path(ioutil.ReadFile(args[1]))
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// pathSecond, err := validate_path(ioutil.ReadFile(args[2]))
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	contentFirst := readFile(firstFilePath)
+	contentSecond := readFile(secondFilePath)
+
+	firstMap := jsonToMap(contentFirst)
+	secondMap := jsonToMap(contentSecond)
+
+}
+
+func readFile(path string) []byte {
+	content, err := os.ReadFile(path)
 	if err != nil {
-		return nil, err
+		log.Fatal("Error when opening file: ", err)
 	}
-	path_second, err := validate_path(filepath.Base(args[2]))
+	fmt.Printf("Successfully opened %s", path)
+	return content
+}
+
+func jsonToMap(content []byte) map[string]interface{} {
+	var payload map[string]interface{}
+	err := json.Unmarshal(content, &payload)
 	if err != nil {
-		return nil, err
+		log.Fatal("Error during Unmarshal(): ", err)
 	}
+	return payload
 }
