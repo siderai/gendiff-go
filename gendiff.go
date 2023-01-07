371b8a6001aeb3601f27cbb7302e206041532ac8
Alexander Sidorov <sidai@bk.ru>
2023-01-07 19:02:28 +0300

    refactor: Error handling

--- gendiff.go
+++ gendiff.go
@@ -12,8 +12,14 @@ func validate_path(path string) (validated_path string, err error) {
 }
 
 func main() {
-	firstFilePath := os.Args[1]
-	secondFilePath := os.Args[2]
+	args := os.Args
+	if len(args) < 3 {
+		fmt.Println("Not enough arguments")
+	} else if len(args) > 3 {
+		fmt.Println("Too many arguments")
+	}
+	firstFilePath := args[1]
+	secondFilePath := args[2]
 
 	// TODO: Validate paths
 
@@ -26,28 +32,39 @@ func main() {
 	// 	return nil, err
 	// }
 
-	contentFirst := readFile(firstFilePath)
-	contentSecond := readFile(secondFilePath)
-
-	firstMap := jsonToMap(contentFirst)
-	secondMap := jsonToMap(contentSecond)
-
-}
-
-func readFile(path string) []byte {
-	content, err := os.ReadFile(path)
+	contentFirst, err := readFile(firstFilePath)
 	if err != nil {
 		log.Fatal("Error when opening file: ", err)
 	}
-	fmt.Printf("Successfully opened %s", path)
-	return content
-}
-
-func jsonToMap(content []byte) map[string]interface{} {
-	var payload map[string]interface{}
-	err := json.Unmarshal(content, &payload)
+	contentSecond, err := readFile(secondFilePath)
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
+	}
+	firstMap, err := jsonToMap(contentFirst)
 	if err != nil {
 		log.Fatal("Error during Unmarshal(): ", err)
 	}
-	return payload
+	secondMap, err := jsonToMap(contentSecond)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
+
+	// parse
+	// replace
+	// calculate diff in goroutine
+}
+
+func readFile(path string) ([]byte, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return content, err
+	}
+	fmt.Printf("Successfully opened %s", path)
+	return content, err
+}
+
+func jsonToMap(content []byte) (map[string]interface{}, error) {
+	var payload map[string]interface{}
+	err := json.Unmarshal(content, &payload)
+	return payload, err
 }
